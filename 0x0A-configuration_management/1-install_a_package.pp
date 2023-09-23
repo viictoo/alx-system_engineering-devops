718c8f06ece435207125102f71b2ba93913e83db
Victor Langat <provicml@gmail.com>
2023-09-23 15:28:54 +0300

    testing command

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,19 @@
 # Using Puppet, install flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# package { 'flask':
+#   ensure   => '2.1.0',
+#   provider => 'pip3',
+# }
+# install flask version 2.1.0 from pip3
+package { 'python3':
+  ensure  => '3.10.12',
+}
+
+package {'python3-pip':
+  ensure  => installed,
+}
+
+exec { 'flask':
+  command => 'pip3 install flask==2.1.0',
+  path    => '/usr/bin',
+  unless  => 'pip3 show flask',
 }
