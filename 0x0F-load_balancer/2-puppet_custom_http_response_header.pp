1ac50f3b1533016581d5bd6adc6b27b099fd918a
Victor Langat <provicml@gmail.com>
2023-10-02 18:04:43 +0300

    no file line

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,14 +1,22 @@
 # using puppet to add a line in a file
 
-# exec { 'update':
-#   command  => 'sudo apt-get -y update',
-#   provider => shell,
-# }
+exec { 'update':
+  command  => 'apt-get -y update',
+  provider => shell,
+}
 
-# exec { 'install_nginx':
-#   command  => 'sudo apt-get -y install nginx',
-#   provider => shell,
-# }
+exec { 'install_nginx':
+  command  => 'apt-get -y install nginx',
+  provider => shell,
+}
+exec { 'install_nginx':
+  command  => 'apt-get -y install nginx',
+  provider => shell,
+}
+exec { 'test':
+  command  => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;',
+  provider => shell,
+}
 
 # file_line { 'add_header':
 #   path   => '/etc/nginx/sites-available/default',
@@ -16,14 +24,7 @@
 #   after  => 'server_name _;',
 # }
 
-# exec { 'restart':
-#   command   => 'service nginx restart',
-#   provider  => shell,
-# }
-exec { 'test':
-  command  => 'apt-get -y update;
-  apt-get -y install nginx;
-  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
-  service nginx restart',
-  provider => shell,
+exec { 'restart':
+  command   => 'service nginx restart',
+  provider  => shell,
 }
