45ade6ed5248afca8cc707a6616328c0e6d2b8ab
Victor Langat <provicml@gmail.com>
2023-10-02 14:25:12 +0300

    append

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -13,8 +13,8 @@ file_line { 'add_header_X-Served-By':
   ensure => present,
   path   => '/etc/nginx/sites-enabled/default',
   line   => "\tadd_header X-Served-By ${hostname};",
-  match  => '^server\s*{',
-  after  => 'matched',
+  # match  => 'dd^server\s*{',
+  after  => '^server\s*{',
 }
 
 service {'nginx':
