d2f2426bf8f6fba5ca4129e7a6f580d9e32738a9
Victor Langat <provicml@gmail.com>
2023-10-02 14:14:45 +0300

    update and restart

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,22 @@
 # using puppet to add a line in a file
+
+exec {'update':
+  command => 'apt-get -y update',
+  path    => '/usr/bin'
+}
+
+package { 'nginx':
+  ensure  => installed,
+}
+
 file_line { 'add_header_X-Served-By':
   ensure => present,
   path   => '/etc/nginx/sites-enabled/default',
   line   => "\tadd_header X-Served-By ${hostname};",
   match  => '^server\s*{',
   after  => 'matched',
+}
+
+service {'nginx':
+  ensure  => 'running'
 }
