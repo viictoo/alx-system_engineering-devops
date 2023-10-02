2417f40ddc23909fb561e96344a43ccdf1ff890d
Victor Langat <provicml@gmail.com>
2023-10-02 17:53:16 +0300

    custom header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,22 @@
 # using puppet to add a line in a file
 
 exec { 'update':
-  command  => 'apt-get -y update',
+  command  => 'sudo apt-get -y update',
   provider => shell,
 }
 
 exec { 'install_nginx':
-  command  => 'apt-get -y install nginx',
+  command  => 'sudo apt-get -y install nginx',
   provider => shell,
 }
 
 file_line { 'add_header':
-  path   => '/etc/nginx/sites-enabled/default',
+  path   => '/etc/nginx/sites-available/default',
   line   => "\tadd_header X-Served-By \"${hostname}\";",
   after  => 'server_name _;',
-  notify => Exec['restart'],
 }
 
 exec { 'restart':
   command   => 'service nginx restart',
   provider  => shell,
-  subscribe => File_line['add_header_X-Served-By'],
 }
