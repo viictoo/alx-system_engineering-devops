fa3b66a7344ce7e764dda953e56c42af0a667ec0
Victor Langat <provicml@gmail.com>
2023-10-02 18:13:34 +0300

    was it linter

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,40 +1,17 @@
 # using puppet to add a line in a file
 
 exec { 'update':
-  command  => 'apt-get -y update',
+  command  => 'apt-get -y update; apt-get -y install nginx',
   provider => shell,
 }
 
-exec { 'install_nginx':
-  command  => 'apt-get -y install nginx',
-  provider => shell,
-}
-exec { 'install_nginx':
-  command  => 'apt-get -y install nginx',
-  provider => shell,
-}
 
 exec { 'test':
   command  => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;',
   provider => shell,
 }
 
-# file_line { 'add_header':
-#   path   => '/etc/nginx/sites-available/default',
-#   line   => "\tadd_header X-Served-By \"${hostname}\";",
-#   after  => 'server_name _;',
-# }
-
 exec { 'restart':
   command  => 'service nginx restart',
   provider => shell,
 }
-
-# # Automation: creates a custom HTTP header response with Puppet.
-# exec { 'command':
-#   command  => 'apt-get -y update;
-#   apt-get -y install nginx;
-#   sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
-#   service nginx restart',
-#   provider => shell,
-# }
