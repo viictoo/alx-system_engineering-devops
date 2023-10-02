0bfba398e8fd79b2a151d723be111d8391762296
Victor Langat <provicml@gmail.com>
2023-10-02 18:18:32 +0300

    two exec

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,3 +4,8 @@ exec { 'update':
   command  => 'apt-get -y update; apt-get -y install nginx; sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default; service nginx restart',
   provider => shell,
 }
+
+exec { 'test':
+  command  => 'echo ""',
+  provider => shell,
+}
