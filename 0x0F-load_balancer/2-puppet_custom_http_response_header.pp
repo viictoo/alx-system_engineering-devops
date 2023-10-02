14eb7231a425db66cf4a69db0bc669f09cfcee38
Victor Langat <provicml@gmail.com>
2023-10-02 13:41:55 +0300

    better work

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,8 @@
+
+file_line { 'add_header_X-Served-By':
+  ensure => present,
+  path   => '/etc/nginx/sites-enabled/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  match  => '^server\s*{',
+  after  => 'matched',
+}
---
+++ 0x0F-load_balancer/README.md
@@ -0,0 +1,4 @@
+# 0x0F. Load balancer
+    Introduction to load-balancing and HAproxy
+    HTTP header
+    Debian/Ubuntu HAProxy packages
