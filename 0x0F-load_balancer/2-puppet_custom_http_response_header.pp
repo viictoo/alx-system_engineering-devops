0669f2abcf8d9ffa9eeca4b447882b794a2081a8
Victor Langat <provicml@gmail.com>
2023-10-02 17:46:07 +0300

    rm ensure

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,30 +1,24 @@
 # using puppet to add a line in a file
+
 exec { 'update':
-  command  => '/usr/bin/apt-get -y update',
+  command  => 'apt-get -y update',
   provider => shell,
 }
 
-exec { 'install nginx':
-  command  => 'sudo apt-get -y install nginx',
+exec { 'install_nginx':
+  command  => 'apt-get -y install nginx',
   provider => shell,
 }
 
-
-file_line { 'add_header_X-Served-By':
-  ensure => present,
+file_line { 'add_header':
   path   => '/etc/nginx/sites-enabled/default',
   line   => "\tadd_header X-Served-By \"${hostname}\";",
   after  => 'server_name _;',
-  # notify => Service['nginx'],
+  notify => Exec['restart'],
 }
 
-# service { 'nginx':service nginx status
-
-#   ensure    => 'running',
-#   enable    => true,
-#   subscribe => File_line['add_header_X-Served-By'],
-# }
-exec { 'command':
-  command  => 'service nginx restart',
-  provider => shell,
+exec { 'restart':
+  command   => 'service nginx restart',
+  provider  => shell,
+  subscribe => File_line['add_header_X-Served-By'],
 }
