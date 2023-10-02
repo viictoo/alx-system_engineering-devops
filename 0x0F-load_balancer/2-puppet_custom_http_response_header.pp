ba05a73710fea183b427958867e5167775491a94
Victor Langat <provicml@gmail.com>
2023-10-02 18:16:38 +0300

    last option

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,17 +1,6 @@
 # using puppet to add a line in a file
 
 exec { 'update':
-  command  => 'apt-get -y update; apt-get -y install nginx',
-  provider => shell,
-}
-
-
-exec { 'test':
-  command  => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;',
-  provider => shell,
-}
-
-exec { 'restart':
-  command  => 'service nginx restart',
+  command  => 'apt-get -y update; apt-get -y install nginx; sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default; service nginx restart',
   provider => shell,
 }
