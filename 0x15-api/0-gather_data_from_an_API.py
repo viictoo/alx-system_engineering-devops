63bcaefd89f40e16163cb7e653b056f5c809e287
Victor Langat <provicml@gmail.com>
2023-10-23 22:57:44 +0300

    test comment

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,72 +1,37 @@
 #!/usr/bin/python3
-"""api module"""
+"""
+Script that, using this REST API,
+for a given employee ID, returns information
+about his/her TODOs list progress.
+"""
 import requests
 from sys import argv as av
 
 
-def validator(av):
-    """
-    validate user input
-    """
-    if len(av) < 2:
-        print("Usage: {} id".format(av[0]))
-        exit(-1)
-    try:
-        int(av[1])
-        return (av[1])
-    except ValueError:
-        print("id must be an integer")
-        exit(-1)
-
-
-def getName(id):
-    """
-    get employee name from id
-    """
-    uri = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
-
-    response = requests.get(uri)
-    employee = (response.json())
-    if employee:
-        return (employee.get("name"))
-    else:
-        print("Not a Valid JSON")
-        exit(-1)
-
-
-def getData(id, name):
+def getData():
     """get and display required data
     """
-
+    id = av[1]
     uri = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id)
+    resp = requests.get(uri).json()
 
-    response = requests.get(uri)
-    resp = (response.json())
-    if resp:
-        try:
-            tasks = []
-            total, completed = 0, 0
-            for task in resp:
-                total += 1
-                if task.get("completed") is True:
-                    tasks.append(task.get("title"))
-                    completed += 1
+    uri = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+    employee = (requests.get(uri).json())
+    name = employee.get("name")
 
-            print("Employee {} is done with tasks({}/{}):"
-                  .format(name, completed, total))
-            for tsk in tasks:
-                print('\t {}'.format(tsk))
+    tasks = []
+    total, completed = 0, 0
+    for task in resp:
+        total += 1
+        if task.get("completed") is True:
+            tasks.append(task.get("title"))
+            completed += 1
 
-        except Exception:
-            print("Some exception occured")
-            return (-1)
-    else:
-        print("Not a Valid JSON")
-        exit(-1)
+    print("Employee {} is done with tasks({}/{}):"
+            .format(name, completed, total))
+    for tsk in tasks:
+        print('\t {}'.format(tsk))
 
 
 if __name__ == "__main__":
-
-    e_id = validator(av)
-    name = getName(e_id)
-    getData(e_id, name)
+    getData()
