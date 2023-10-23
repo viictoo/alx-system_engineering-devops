458bacd8a26c65b5a63f85416a2163016be683f8
Victor Langat <provicml@gmail.com>
2023-10-23 13:08:40 +0300

    str id

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -46,7 +46,7 @@ def getAllData(employees):
                             "username": employee[1]}
                         row.append(dic)
 
-                jDic[int(employee[0])] = row
+                jDic[str(employee[0])] = row
 
             filename = "todo_all_employees.json"
 
