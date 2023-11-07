5456a30b9e33a1c970b2c64c3843fe9d877b467a
Victor Langat <provicml@gmail.com>
2023-11-07 14:46:16 +0300

    test print None

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,7 +11,7 @@ def top_ten(subreddit):
 
     """
     if subreddit is None or type(subreddit) is not str:
-        return 0
+        print(None)
 
     headers = {
         'User-Agent': 'VUTHhzWkxrTG56djBsR3dhN',
@@ -20,9 +20,10 @@ def top_ten(subreddit):
 
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
 
-    response = requests.get(url, headers=headers, params=parameters)
+    response = requests.get(url, headers=headers, params=parameters,
+                            allow_redirects=False)
     if response.status_code == 404:
-        return 0
+        print(None)
     data = response.json()
 
     posts = data.get('data').get('children')
