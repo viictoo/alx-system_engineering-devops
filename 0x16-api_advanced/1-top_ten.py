05219ef25634f2e60d8f4b140e69f6da19e728a8
Victor Langat <provicml@gmail.com>
2023-11-07 14:58:28 +0300

    test the other

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,6 +10,9 @@ def top_ten(subreddit):
     Args: https://www.reddit.com/r/python/top.json?limit=10
 
     """
+    if type(subreddit) is not str:
+        print(None)
+
     headers = {'User-Agent': 'VUTHhzWkxrTG56djBsR3dhN'}
     parameters = {'limit': 10}
 
