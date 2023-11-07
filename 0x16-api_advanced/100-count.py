863f6d2f61700a6f2f8b3f92d7d9e3c527043dc6
Victor Langat <provicml@gmail.com>
2023-11-07 17:43:56 +0300

    test no words

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -4,19 +4,22 @@ import requests
 
 
 def count_words(subreddit, word_list, words=None, after=None):
-    """ a function that queries the Reddit API and prints the
-    titles of the first 10 hot posts listed for a given subreddit
+    """a recursive function that queries the Reddit API, parses
+    the title of all hot articles, and prints a sorted count of
+    given keywords (case-insensitive, delimited by spaces
 
-    Args: https://www.reddit.com/r/python/top.json?limit=10
+    url: https://www.reddit.com/r/programming/hot.json
 
     """
     if subreddit is None or type(subreddit) is not str:
-        print(None)
+        return
+    if word_list is None or len(word_list) == 0:
+        return
+
     if words is None:
         words = {}
         for word in word_list:
             word_lower = word.lower()
-
             if word_lower in words:
                 words[word_lower]['count'] += 1
             else:
