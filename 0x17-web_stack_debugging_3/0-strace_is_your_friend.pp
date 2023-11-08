03aafc5d7635b1b3660b7d7209f5ecece03aeb80
Victor Langat <provicml@gmail.com>
2023-11-08 17:03:00 +0300

    remove typo

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -4,8 +4,3 @@ exec { 'replace':
   command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   path    => '/usr/bin',
 }
-
-exec { 'restart':
-  path    => '/usr/bin/',
-  command => 'sudo service apache2 restart',
-}
